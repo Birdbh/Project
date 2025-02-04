5c084b8b2e2acf45b7518ba87bc5c63e876aca2e
Birdbh <birdlkjlkj123@gmail.com>
2025-02-04 13:32:26 -0800

    test file to figure out where the nodes are

--- src/coms/config.py
+++ src/coms/config.py
@@ -1,5 +1,26 @@
 node_dictionary = {
+    "opc.tcp://172.21.10.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."xCurConvStatus"']
+    },
+    "opc.tcp://172.21.1.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
+    "opc.tcp://172.21.2.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
     "opc.tcp://172.21.3.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
+    "opc.tcp://172.21.4.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
+    "opc.tcp://172.21.6.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
+    "opc.tcp://172.21.7.1:4840" : {
+        "nodes" : ['ns=3;s="Datablock1"."Tag1"']
+    },
+    "opc.tcp://172.21.9.1:4840" : {
         "nodes" : ['ns=3;s="Datablock1"."Tag1"']
     }
 }
