22f4903dc308299da5f3665c2769cccd4f6835ea
Birdbh <birdlkjlkj123@gmail.com>
2025-02-11 13:14:29 -0800

    trial tags and xml

--- src/coms/config.py
+++ src/coms/config.py
@@ -1,6 +1,16 @@
 node_dictionary = {
     "opc.tcp://172.21.10.1:4840" : {
-        "nodes" : ['ns=3;s="xCurConvStatus"']
+        "nodes" : ['ns=3;s="xCurConvStatus"', 
+        'ns=3;s=V_"ActuatorCntr"."Drive"."iConvVelo"',
+        'ns=3;s=O_"DriveDC"."WarnConvStop"',
+        'ns=3;s=V_"DriveDC"."xCarrierOnConv"',
+        'ns=3;s=O_"Stopper_Default"."fbWarnConvOccDef"',
+        'ns=3;s=V_"stAppOutputs"."xConvStop"',
+        'ns=3;s=V_"stOptions"."xEnConvStop"',
+        'ns=3;s="dbVar"."Options"."xEnConvStop"',
+        'ns=3;s="dbAppIF"."Out"."xConvStop"',
+        'ns=3;s="dbTransport"."Drive1"."xEnConvStop"',
+        'ns=3;s="dbTransport"."Drive1"."xCarrierOnConv"']
     },
     "opc.tcp://172.21.1.1:4840" : {
         "nodes" : ['ns=3;s="xCurConvStatus"']
